17e19ebc8c4aa6e859191d5f92314288116fd859
Jonathan Morgan <jonathan.morgan.007@gmail.com>
2013-04-09 11:02:43 -0500

    2013-04-09 - Jonathan Morgan - added StringHelper class.

---
+++ strings/string_helper.py
@@ -0,0 +1,48 @@
+# Imports
+import htmlentitydefs
+
+# define EmailHelper class.
+class StringHelper( object ):
+
+
+    @staticmethod
+    def unicode_escape( string_IN ):
+        
+        """
+        Tidys up unicode entities into HTML friendly entities
+    
+        Takes a unicode string as an argument
+    
+        Returns a unicode string
+        """
+    
+        # return reference
+        string_OUT = ""
+        
+        # declare variables
+        char = ""
+        name = ""
+        entity = ""
+    
+        for char in string_IN:
+
+            if ord( char ) in htmlentitydefs.codepoint2name:
+
+                name = htmlentitydefs.codepoint2name.get( ord( char ) )
+                entity = htmlentitydefs.name2codepoint.get( name )
+                string_OUT +="&#" + str(entity)
+    
+            else:
+
+                string_OUT += char
+                
+            #-- END check to see if character has an entity --#
+            
+        #-- END loop over all characters in string_IN --#
+    
+        return string_OUT
+    
+    #-- END unicode_escape() function --#
+
+
+#-- END class StringHelper --#
