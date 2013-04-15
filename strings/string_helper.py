2617485ecc11a3767b6a2e92978b42cbb5259bf4
Jonathan Morgan <jonathan.morgan.007@gmail.com>
2013-04-15 01:09:41 -0500

    2013-04-15 - Jonathan Morgan - mail SMTP supports SSL, username and password. Added method to StringHelper to strip out HTML.

--- strings/string_helper.py
+++ strings/string_helper.py
@@ -1,7 +1,66 @@
 # Imports
 import htmlentitydefs
+from HTMLParser import HTMLParser
 
-# define EmailHelper class.
+# define MLStripper class (from: http://stackoverflow.com/questions/753052/strip-html-from-strings-in-python )
+class MLStripper(HTMLParser):
+    def __init__(self):
+        self.reset()
+        self.fed = []
+    def handle_data(self, d):
+        self.fed.append(d)
+    def get_data(self):
+        return ''.join(self.fed)
+
+    @classmethod
+    def strip_tags( cls, html_IN ):
+        s = cls()
+        s.feed( html_IN )
+        return s.get_data()
+        
+#-- END class MLStripper --#
+
+
+# define HTMLTextExtractor class (from: http://stackoverflow.com/questions/753052/strip-html-from-strings-in-python )
+class HTMLTextExtractor( HTMLParser ):
+
+    def __init__(self):
+        HTMLParser.__init__(self)
+        self.result = [ ]
+
+    def handle_data(self, d):
+        self.result.append(d)
+
+    def handle_charref(self, number):
+        codepoint = int(number[1:], 16) if number[0] in (u'x', u'X') else int(number)
+        self.result.append(unichr(codepoint))
+
+    def handle_entityref(self, name):
+        codepoint = htmlentitydefs.name2codepoint[name]
+        self.result.append(unichr(codepoint))
+
+    def get_text(self):
+        return u''.join(self.result)
+
+    # class method to actually strip text.
+    @classmethod
+    def html_to_text( cls, html_IN = "" ):
+
+        # return reference
+        value_OUT = ""
+
+        s = cls()
+        s.feed( html_IN )
+        value_OUT = s.get_text()
+
+        return value_OUT
+
+    #-- END class method html_to_text --#
+        
+#-- END class HTMLTextExtractor --#
+
+
+# define StringHelper class.
 class StringHelper( object ):
 
 
@@ -43,6 +102,31 @@ class StringHelper( object ):
         return string_OUT
     
     #-- END unicode_escape() function --#
+
+
+    @staticmethod
+    def remove_html( string_IN, preserve_entities_IN = False ):
+        
+        """
+        strips out HTML from string.
+        """
+    
+        # return reference
+        string_OUT = ""
+        
+        if ( preserve_entities_IN == True ):
+        
+            string_OUT = HTMLTextExtractor.html_to_text( string_IN )
+        
+        else:
+
+            string_OUT = MLStripper.strip_tags( string_IN )
+            
+        #-- END check to see if we preserve entities or not. --#
+
+        return string_OUT
+    
+    #-- END remove_html() function --#
 
 
 #-- END class StringHelper --#
