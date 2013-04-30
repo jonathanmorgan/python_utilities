199f9f28f8d0bb5f7c46d7638bf63e0f0ccbcedc
Jonathan Morgan <jonathan.morgan.007@gmail.com>
2013-04-30 11:15:09 -0500

    2013-04-30 - Jonathan Morgan - more nuanced encoding and decoding (more for my memory than for needing separate methods), and made django child class that uses django methods instead of base python methods for decoding.

---
+++ django_utils/django_string_helper.py
@@ -0,0 +1,50 @@
+# python_utilities includes.
+import python_utilities.strings.string_helper
+
+# django includes
+import django.utils.encoding
+
+class DjangoStringHelper( strings.string_helper.StringHelper ):
+    
+
+    #============================================================================
+    # class methods
+    #============================================================================
+
+
+    @classmethod
+    def convert_to_unicode( cls, string_IN, encoding_IN = "", *args, **kwargs ):
+        
+        """
+        Converts string to unicode.  Uses django.utils.encoding.smart_text
+           instead of unicode().
+        
+        Based in part on:
+        http://stackoverflow.com/questions/15800185/unicodeencodeerror-ascii-codec-cant-encode-character-u-xe9
+        http://nedbatchelder.com/text/unipain.html
+        """
+    
+        # return reference
+        string_OUT = ""
+        
+        # decode from external encoding to unicode (probably is UTF-8).
+
+        # dp we have an encoding passed in?
+        if ( ( encoding_IN ) and ( encoding_IN != None ) and ( encoding_IN != "" ) ):
+        
+            # encoding passed in.  Use it.
+            string_OUT = django.utils.encoding.smart_text( string_IN, encoding_IN )
+        
+        else:
+            
+            # no encoding - use default.
+            string_OUT = django.utils.encoding.smart_text( string_IN )
+            
+        #-- END see if encoding is passed in. --#
+        
+        return string_OUT
+    
+    #-- END convert_to_unicode() function --#
+
+
+#-- END class DjangoStringHelper --#
--- strings/string_helper.py
+++ strings/string_helper.py
@@ -64,6 +64,20 @@ class HTMLTextExtractor( HTMLParser ):
 class StringHelper( object ):
 
 
+    #----------------------------------------------------------------------
+    # constants-ish
+    #----------------------------------------------------------------------
+
+
+    ENCODING_ASCII = 'ascii'
+    ENCODING_UTF8 = 'utf-8'
+
+
+    #============================================================================
+    # static methods
+    #============================================================================
+
+
     @staticmethod
     def unicode_escape( string_IN ):
         
@@ -105,48 +119,6 @@ class StringHelper( object ):
 
 
     @staticmethod
-    def unicode_to_ascii( string_IN, encoding_IN = "", encode_error_IN = "xmlcharrefreplace" ):
-        
-        """
-        Converts string to unicode, then to ascii, converting any Unicode 
-           characters to XML entities.  UTF-8 is not unicode - it is an
-           encoding, just like ASCII.  To really deal with ENCODING problems,
-           you need to first DEECODE to unicode.  Who knew?
-        
-        Based in part on:
-        http://stackoverflow.com/questions/15800185/unicodeencodeerror-ascii-codec-cant-encode-character-u-xe9
-        http://nedbatchelder.com/text/unipain.html
-        """
-    
-        # return reference
-        string_OUT = ""
-        
-        # first decode from external encoding to unicode (probably is UTF-8).
-
-        # dp we have an encoding passed in?
-        if ( ( encoding_IN ) and ( encoding_IN != None ) and ( encoding_IN != "" ) ):
-        
-            # encoding passed in.  Use it.
-            string_OUT = unicode( string_IN, encoding_IN )
-        
-        else:
-            
-            # no encoding - use default.
-            string_OUT = unicode( string_IN )
-            
-        #-- END see if encoding is passed in. --#
-        
-        # then, encode to ascii, replacing any non-ascii characters with
-        #    error strategy passed in (defaults to converting them to XML
-        #    entities).
-        string_OUT = string_OUT.encode( 'ascii', encode_error_IN )
-                
-        return string_OUT
-    
-    #-- END unicode_to_ascii() function --#
-
-
-    @staticmethod
     def remove_html( string_IN, preserve_entities_IN = False ):
         
         """
@@ -169,6 +141,122 @@ class StringHelper( object ):
         return string_OUT
     
     #-- END remove_html() function --#
+
+
+    #============================================================================
+    # class methods
+    #============================================================================
+
+    
+    @classmethod
+    def convert_to_unicode( cls, string_IN, encoding_IN = "", *args, **kwargs ):
+        
+        """
+        Converts string to unicode
+        
+        Based in part on:
+        http://stackoverflow.com/questions/15800185/unicodeencodeerror-ascii-codec-cant-encode-character-u-xe9
+        http://nedbatchelder.com/text/unipain.html
+        """
+    
+        # return reference
+        string_OUT = ""
+        
+        # decode from external encoding to unicode (probably is UTF-8).
+
+        # dp we have an encoding passed in?
+        if ( ( encoding_IN ) and ( encoding_IN != None ) and ( encoding_IN != "" ) ):
+        
+            # encoding passed in.  Use it.
+            string_OUT = unicode( string_IN, encoding_IN )
+        
+        else:
+            
+            # no encoding - use default.
+            string_OUT = unicode( string_IN )
+            
+        #-- END see if encoding is passed in. --#
+        
+        return string_OUT
+    
+    #-- END convert_to_unicode() function --#
+
+
+    @classmethod
+    def encode_string( cls, string_IN, output_encoding_IN = 'ascii', input_encoding_IN = '', encode_error_IN = "xmlcharrefreplace", *args, **kwargs ):
+        
+        '''
+        Accepts a string that might or might not play well with ascii.  First,
+           tries to convert to ascii.  If exception, decodes it to unicode, then
+           re-encodes it into the requested output_encoding, escaping illegal
+           characters to XML entities by default.
+           
+        Parameters:
+        - string_IN - string we want to safen for the output encoding specified.
+        - output_encoding_IN - encoding we want this string to be safe for.  Defaults to ascii.
+        - input_encoding_IN - encoding in which our string is encoded.
+        - encod_error_IN - what we want to do on encoding errors, when converting to safe string (default is "xmlcharrefreplace", which convers those characters to entities).
+        '''
+        
+        # return reference
+        string_OUT = ""
+        
+        # declare variables
+        unicode_string = ""
+        
+        # first, store string_IN in string_OUT.
+        string_OUT = string_IN
+        
+        # make sure it is not None or empty string    
+        if ( ( string_OUT ) and ( string_OUT != None ) and ( string_OUT != "" ) ):
+        
+            # first, see if converting to desired encoding breaks things.
+            try:
+            
+                string_OUT.decode( output_encoding_IN )
+                
+            except:
+    
+                # yes, it does.  Encode differently.
+                
+                # Convert to unicode.
+                unicode_string = cls.convert_to_unicode( string_OUT, input_encoding_IN )
+                
+                # then, encode to desired encoding, escaping invalid characters.
+                string_OUT = unicode_string.encode( output_encoding_IN, encode_error_IN )
+                
+            #-- END check to see if we need to safen string. --#
+            
+        #-- END check to see if any flair text - don't want "None". --#
+        
+        return string_OUT
+        
+    #-- END method safe_string --#
+
+
+    @classmethod
+    def unicode_to_ascii( cls, string_IN = None, encoding_IN = "", encode_error_IN = "xmlcharrefreplace", *args, **kwargs ):
+        
+        """
+        Converts string to unicode, then to ascii, converting any Unicode 
+           characters to XML entities.  UTF-8 is not unicode - it is an
+           encoding, just like ASCII.  To really deal with ENCODING problems,
+           you need to first DEECODE to unicode.  Who knew?
+        
+        Based in part on:
+        http://stackoverflow.com/questions/15800185/unicodeencodeerror-ascii-codec-cant-encode-character-u-xe9
+        http://nedbatchelder.com/text/unipain.html
+        """
+    
+        # return reference
+        string_OUT = ""
+
+        # use safe string to convert to ascii.        
+        string_OUT = cls.encode_string( string_IN, 'ascii', encode_error_IN = encode_error_IN )
+                
+        return string_OUT
+    
+    #-- END unicode_to_ascii() function --#
 
 
 #-- END class StringHelper --#
