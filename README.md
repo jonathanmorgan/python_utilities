11f465e83ae0534fc1e5d960be1b3228e5c825df
Jonathan Morgan <jonathan.scott.morgan@gmail.com>
2013-08-20 15:49:35 -0400

    2013-08-20 - Jonathan Morgan - implemented URL redirect following in http_helper and am finally getting unicode and ASCII figured out in string_helper.

--- README.md
+++ README.md
@@ -2,14 +2,15 @@
 
 Python Utility classes.  Includes the following:
 
-- __/beautiful\_soup/beautiful\_soup\_helper.py__ - BeautifulSoupHelper class that implements helper methods for common things you do with BeautifulSoup, like getting child text and encoding HTML entities.  Built against BeautifulSoup 3, updated to import BeautifulSoup 4, not sure if it works at the moment.  Should work just fine...
+- __/beautiful\_soup/beautiful\_soup\_helper.py__ - BeautifulSoupHelper class that implements helper methods for common things you do with BeautifulSoup, like getting child text and encoding HTML entities.  Built against BeautifulSoup 3, updated to import BeautifulSoup 4, work just fine far as I can tell...
 - __/booleans/boolean\_helper.py__ - BooleanHelper class with method to convert non-boolean values to boolean type based on valid known true values (1, 't', 'true', 'y', 'yes').
 - __/database/MySQLdb\_helper.py__ - MySQLdb\_Helper class encapsulates basic logic for dealing with creating connections and cursors using the MySQLdb library.  Not fancy.  Opens and closes, nothing more.
 - __/dictionaries/dict\_helper.py__ - for now, just contains a function to retrieve a dict value that also accepts a default, so you can define default yourself when you look things up in a dict.
 - __/django\_utils/queryset\_helper.py__ - QuerySetHelper class that contains memory-efficient ways of iterating over large QuerySets, and also a few convenience methods for adding date and primary key filters to a QuerySet.
 - __/email/email\_helper.py__ - EmailHelper class that contains logic for setting up SMTP server using smtplib, then sending text or HTML email messages.
 - __/exceptions/exception\_helper.py__ - ExceptionHelper class that contains logic for printing exception messages, and also for emailing a summary if email is set up in the isntance.
-- __/network/http\_helper.py__ - Http_Helper class that contains logic for setting checking if a URL has been redirected, and if so, stores status code and URLs.
+- __/network/http\_helper.py__ - Http_Helper class that contains logic for checking if a URL has been redirected, and if so, storing redirect information including status code and redirect URLs.
+- __/network/mechanize\_tools.py__ - Contains logic to support Http_Helper, based on the Dive Into Python site (http://www.diveintopython.net/download/diveintopython-examples-5.4.zip), but using Mechanize's version of urllib2.
 - __/network/openanything.py__ - Contains logic to support Http_Helper, from the Dive Into Python site (http://www.diveintopython.net/download/diveintopython-examples-5.4.zip)
 - __/objects/object\_helper.py__ - ObjectHelper class contains logic for detecting attributes in a given class (like the vars() method, only a little fancier).
 - __/rate\_limited/basic\_rate\_limited.py__ - BasicRateLimited is a non-parallel parent class that contains variables and code for rate-limiting.  Details on extending TK below, in Usage Section.
@@ -19,10 +20,23 @@ Python Utility classes.  Includes the following:
 
 Clone this repository and place it somewhere in your PYTHON\_PATH, including the base "python\_utilities" directory.  The easiest way to use these libraries with a Django site is to clone this repository into the site's folder alongside other applications, so these utilities are a part of the same python path as other django apps.  These utilities are used by other of my django applications, as well.  They can also be used outside of django.
 
+### /beautiful\_soup/*
+
+Requires the Beautiful Soup 4 package, installed via pip:
+
+    (sudo) pip install BeautifulSoup4
+    
+If you are planning on using Beautiful Soup's "UnicodeDammit" class, you also should install chardet and/or cchardet:
+
+    (sudo) pip install chardet
+    (sudo) pip install cchardet
+
 ### /database/MySQLdb\_helper.py
 
 Before you can connect to MySQL with this code, you need to do the following:
+
 - install the MySQL client if it isn't already installed.  On linux, you'll also need to install a few dev packages (python-dev, libmysqlclient-dev) ( [source](http://codeinthehole.com/writing/how-to-set-up-mysql-for-python-on-ubuntu/) ).
+
 - install the MySQLdb python package.  To install, you can either install through your operating system's package manager (ubuntu, for example, has package "python-mysqldb") or using pip (`sudo pip install MySQL-python`).
 
 ### /network/*
@@ -30,6 +44,12 @@ Before you can connect to MySQL with this code, you need to do the following:
 Requires you install mechanize, a library that implements a browser client in python:
 
     (sudo) pip install mechanize
+    
+### /strings/*
+
+Requires you to install the "six" package, which helps make python code that can run in either python 2 or 3:
+
+    (sudo) pip install six
 
 ## Usage
 
@@ -45,7 +65,7 @@ For a class you want to be rate-limited:
 
 ## ToDo:
 
-- Implement a Postgresql_Helper class, same interface as MySQL_helper, so they are interchangable, then maybe a factory for getting instances.
+- Implement a Postgresql\_Helper class, same interface as MySQL\_helper, so they are interchangable, then maybe a factory for getting instances.
 
 ## License:
 
--- network/http_helper.py
+++ network/http_helper.py
@@ -64,6 +64,12 @@ class Http_Helper( object ):
     
     # DEBUG - changed to instance variable.
     #DEBUG_FLAG = False
+    HEADER_NAME_USER_AGENT = "User-Agent"
+    HEADER_NAME_ACCEPT = "Accept"
+    HEADER_NAME_ACCEPT_CHARSET = "Accept-Charset"
+    HEADER_NAME_ACCEPT_ENCODING = "Accept-Encoding"
+    HEADER_NAME_ACCEPT_LANGUAGE = "Accept-Language"
+    HEADER_NAME_CONNECTION = "Connection"
 
 
     #============================================================================
@@ -75,6 +81,17 @@ class Http_Helper( object ):
     redirect_status_list = []
     redirect_url_list = []
     
+    # HTTP header values
+    header_user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:22.0) Gecko/20100101 Firefox/22.0"
+    header_accept = 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'
+    header_accept_charset = 'ISO-8859-1,utf-8;q=0.7,*;q=0.3'
+    header_accept_encoding = 'none'
+    header_accept_language = 'en-US,en;q=0.8'
+    header_connection = 'keep-alive'
+    
+    # header dict
+    header_dict = None
+
     # debug_flag
     debug_flag = False
     
@@ -94,6 +111,17 @@ class Http_Helper( object ):
         self.redirect_status_list = []
         self.redirect_url_list = []
     
+        # HTTP header values
+        self.header_user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:22.0) Gecko/20100101 Firefox/22.0"
+        self.header_accept = 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'
+        self.header_accept_charset = 'ISO-8859-1,utf-8;q=0.7,*;q=0.3'
+        self.header_accept_encoding = 'none'
+        self.header_accept_language = 'en-US,en;q=0.8'
+        self.header_connection = 'keep-alive'
+        
+        # header dict
+        self.header_dict = None
+    
         # debug
         self.debug_flag = False
 
@@ -105,7 +133,85 @@ class Http_Helper( object ):
     #============================================================================
     
 
+    def get_http_header_dict( self, header_dict_IN = None, *args, **kwargs ):
+        
+        '''
+        Accepts a set of header params in a dict.  Looks for a pre-existing dict
+           in this instance.  If one present, retrieves it.  If not, creates one.
+           If creating a new header dict, first initializes values from header
+           variables in this instance.  Then, if dict passed in, places those
+           values in the dict, overwriting any that are both in the nested set
+           and in the set passed in with those passed in.  Stores the resulting
+           dict, and returns it.
+           
+        If you want to alter the defaults, you have two options - you can either
+           change the values in the "header_*" variables, or you can create a
+           dict to hold your values and pass that to this method.  If you want to
+           set values not stored in "header_*" variables, then you'll have to
+           either pass in a header dict or just craft your header dict as you
+           want it and store it in the header_dict instance variable in this
+           instance.
+        
+        Parameters:
+        - header_dict_IN - set of name-value HTTP header variables you want added to the header dict that is returned.
+        '''
+        
+        # return reference
+        dict_OUT = None
+        
+        # see if there is already a header dict nested.
+        if ( ( self.header_dict ) and ( self.header_dict != None ) and ( len( self.header_dict ) > 0 ) ):
+        
+            # we have one.  Return it.
+            dict_OUT = header_dict
+        
+        else:
+        
+            # nothing.  Make a new one.
+            dict_OUT = {}
+            dict_OUT[ self.HEADER_NAME_USER_AGENT ] = self.header_user_agent
+            dict_OUT[ self.HEADER_NAME_ACCEPT ] = self.header_accept
+            dict_OUT[ self.HEADER_NAME_ACCEPT_CHARSET ] = self.header_accept_charset
+            dict_OUT[ self.HEADER_NAME_ACCEPT_ENCODING ] = self.header_accept_encoding
+            dict_OUT[ self.HEADER_NAME_ACCEPT_LANGUAGE ] = self.header_accept_language
+            dict_OUT[ self.HEADER_NAME_CONNECTION ] = self.header_connection
+        
+        #-- END check to see if header dict exists. --#
+        
+        # Got a dict of values passed in?
+        if ( ( header_dict_IN ) and ( header_dict_IN != None ) and ( len( header_dict_IN ) > 0 ) ):
+
+            # got one.  Put its data in the dict we are passing out.
+            dict_OUT.update( header_dict_IN )
+        
+        #-- END check to see if header dict passed in. --#
+        
+        return dict_OUT
+        
+    #-- END method get_http_header_dict() --#
+
+
     def get_redirect_url( self, url_IN, *args, **kwargs ):
+    
+        '''
+        method defaults to calling mechanize methods, not urllib2.  If this
+           becomes a problem, could do fancy things here to choose which is
+           called.
+        '''
+        
+        # return reference
+        url_OUT = ""
+        
+        # call method implementation.
+        url_OUT = self.get_redirect_url_mechanize( url_IN, args, kwargs )
+        
+        return url_OUT
+        
+    #-- END method get_redirect_url() --#
+    
+
+
+    def get_redirect_url_urllib2( self, url_IN, *args, **kwargs ):
     
         '''
         Accepts a URL.  Tries to load that page.  If page loads, checks to see if
@@ -119,6 +225,7 @@ class Http_Helper( object ):
         url_OUT = None
     
         # declare variables.
+        headers = None
         request = None
         opener = None
         open_result = None
@@ -126,14 +233,8 @@ class Http_Helper( object ):
         # got a url?
         if ( ( url_IN ) and ( url_IN != None ) and ( url_IN != "" ) ):
 
-            # create request for a URL (must include a protocol - http://, etc.).
-            request = urllib2.Request( url_IN )
-        
-            # make an opener, passing it an instance of our SmartRedirectHandler()
-            opener = urllib2.build_opener( openanything.SmartRedirectHandler() )
-            
-            # open the URL
-            open_result = opener.open(request)
+            # load the URL
+            open_result = self.load_url_urllib2( url_IN, args, kwargs )
             
             # if redirected, there will be a status_list attribute
             if ( hasattr( open_result, "status_list" ) == True ):
@@ -165,7 +266,7 @@ class Http_Helper( object ):
                 
         return url_OUT
     
-    #-- END methot get_redirect_url --#    
+    #-- END methot get_redirect_url_urllib2 --#    
 
 
     def get_redirect_url_mechanize( self, url_IN, *args, **kwargs ):
@@ -189,14 +290,8 @@ class Http_Helper( object ):
         # got a url?
         if ( ( url_IN ) and ( url_IN != None ) and ( url_IN != "" ) ):
 
-            # create request for a URL (must include a protocol - http://, etc.).
-            request = mechanize.Request( url_IN )
-        
-            # make an opener, passing it an instance of our SmartRedirectHandler()
-            opener = mechanize.build_opener( mechanize_tools.SmartRedirectHandler() )
-            
-            # open the URL
-            open_result = opener.open( request )
+            # load URL
+            open_result = self.load_url_mechanize( url_IN, args, kwargs )
             
             # if redirected, there will be a status_list attribute
             if ( hasattr( open_result, "status_list" ) == True ):
@@ -229,6 +324,117 @@ class Http_Helper( object ):
         return url_OUT
     
     #-- END methot get_redirect_url_mechanize --#    
+
+
+    def load_url( self, url_IN, *args, **kwargs ):
+    
+        '''
+        method defaults to calling mechanize methods, not urllib2.  If this
+           becomes a problem, could do fancy things here to choose which is
+           called.
+        '''
+        
+        # return reference
+        response_OUT = ""
+        
+        # call method implementation.
+        response_OUT = self.load_url_mechanize( url_IN, args, kwargs )
+        
+        return response_OUT
+        
+    #-- END method load_url() --#
+    
+
+
+    def load_url_urllib2( self, url_IN, *args, **kwargs ):
+    
+        '''
+        Accepts a URL.  Tries to load that page.  If page loads, checks to see if
+           redirect. If yes, returns URL to which we were redirected and stores
+           the list of redirect codes in self.redirect_status_list.  If no,
+           returns None.  If there is an error loading the page, will throw an
+           exception.  Return of None DOES NOT imply error in this case.
+        '''
+    
+        # return reference
+        response_OUT = None
+    
+        # declare variables.
+        headers = None
+        request = None
+        opener = None
+        open_result = None
+        
+        # got a url?
+        if ( ( url_IN ) and ( url_IN != None ) and ( url_IN != "" ) ):
+
+            # get header dict
+            headers = self.get_http_header_dict()
+
+            # create request for a URL (must include a protocol - http://, etc.).
+            request = urllib2.Request( url_IN, headers = headers )
+        
+            # make an opener, passing it an instance of our SmartRedirectHandler()
+            opener = urllib2.build_opener( openanything.SmartRedirectHandler() )
+            
+            # open the URL
+            response_OUT = opener.open(request)
+            
+        else:
+        
+            # No URL passed in, return None.
+            response_OUT = None
+        
+        #-- END check to see of URL string passed in. --#            
+                
+        return response_OUT
+    
+    #-- END methot load_url_urllib2 --#    
+
+
+    def load_url_mechanize( self, url_IN, *args, **kwargs ):
+    
+        '''
+        Accepts a URL.  Tries to load that page.  If page loads, checks to see if
+           redirect. If yes, returns URL to which we were redirected and stores
+           the list of redirect codes in self.redirect_status_list.  If no,
+           returns None.  If there is an error loading the page, will throw an
+           exception.  Return of None DOES NOT imply error in this case.
+        '''
+    
+        # return reference
+        response_OUT = None
+    
+        # declare variables.
+        request = None
+        opener = None
+        open_result = None
+        
+        # got a url?
+        if ( ( url_IN ) and ( url_IN != None ) and ( url_IN != "" ) ):
+
+            # get header dict
+            headers = self.get_http_header_dict()
+
+            # create request for a URL (must include a protocol - http://, etc.).
+            request = mechanize.Request( url_IN, headers = headers )
+        
+            # make an opener, passing it an instance of our SmartRedirectHandler()
+            opener = mechanize.build_opener( mechanize_tools.SmartRedirectHandler() )
+            
+            # open the URL
+            response_OUT = opener.open( request )
+            
+        else:
+        
+            # No URL passed in, return None.
+            response_OUT = None
+        
+        #-- END check to see of URL string passed in. --#            
+                
+        return response_OUT
+    
+    #-- END methot load_url_mechanize --#    
 
 
 #-- END class Http_Helper --#
