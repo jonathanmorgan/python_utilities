b2bcaeede15d3a04ad6e254be9cd7e120e919770
Jonathan Morgan <jonathan.scott.morgan@gmail.com>
2016-07-12 18:42:45 -0400

    made DictHelper instantiable, added support for float and decimal.Decimal values.

--- dictionaries/dict_helper.py
+++ dictionaries/dict_helper.py
@@ -25,6 +25,8 @@ along with http://github.com/jonathanmorgan/python_utilities.  If not, see
 # imports
 #================================================================================
 
+# standard library
+import decimal
 
 import six # help with supporting both python 2 and 3.
 
@@ -115,6 +117,83 @@ class DictHelper( object ):
 
 
     @classmethod
+    def get_dict_value_as_decimal( cls, dict_IN, name_IN, default_IN = decimal.Decimal( -1 ) ):
+    
+        '''
+        Accepts dictionary, name, and optional default value (if no default
+           provided, default is -1).  If dictionary or name missing, returns
+           default.  If name not present in dictionary, returns default.  If name
+           in dictionary, returns whatever is mapped to name, converted to a decimal.Decimal instance
+           through call to decimal.Decimal().
+           
+        Parameters:
+        - dict_IN - dictionary we are looking in.
+        - name_IN - name we are looking for in dictionary.
+        - default_IN (defaults to -1) - default value to return if problem or not found in dict.
+        
+        Returns:
+        - value_OUT - value mapped to name_IN in dict_IN, converted to int(), else the default value if problems or if not found in dict.
+        '''
+    
+        # return reference
+        value_OUT = None
+        
+        # first, get value.
+        value_OUT = cls.get_dict_value( dict_IN, name_IN, default_IN )
+        
+        # convert to int unless value is None.
+        if ( value_OUT != None ):
+
+            # value is not None.
+            value_OUT = decimal.Decimal( value_OUT )
+            
+        #-- END check to see if default was None --#
+        
+        return value_OUT
+    
+    #-- END function get_dict_value_as_decimal --#
+
+
+    @classmethod
+    def get_dict_value_as_float( cls, dict_IN, name_IN, default_IN = float( -1 ) ):
+    
+        '''
+        Accepts dictionary, name, and optional default value (if no default
+           provided, default is -1).  If dictionary or name missing, returns
+           default.  If name not present in dictionary, returns default.  If name
+           in dictionary, returns whatever is mapped to name, converted to a
+           float through call to float().
+           
+        Parameters:
+        - dict_IN - dictionary we are looking in.
+        - name_IN - name we are looking for in dictionary.
+        - default_IN (defaults to None) - default value to return if problem or not found in dict.
+        
+        Returns:
+        - value_OUT - value mapped to name_IN in dict_IN, converted to int(), else the default value if problems or if not found in dict.
+        '''
+    
+        # return reference
+        value_OUT = None
+        
+        # first, get value.
+        value_OUT = cls.get_dict_value( dict_IN, name_IN, default_IN )
+        
+        # convert to float unless value is None.
+        if ( value_OUT != None ):
+
+            # value is not None.
+            value_OUT = float( value_OUT )
+            
+        #-- END check to see if default was None --#
+        
+        return value_OUT
+    
+    #-- END function get_dict_value_as_float --#
+
+
+
+    @classmethod
     def get_dict_value_as_int( cls, dict_IN, name_IN, default_IN = -1 ):
     
         '''
@@ -139,8 +218,13 @@ class DictHelper( object ):
         # first, get value.
         value_OUT = cls.get_dict_value( dict_IN, name_IN, default_IN )
         
-        # convert to int.
-        value_OUT = int( value_OUT )
+        # convert to int unless value is None.
+        if ( value_OUT != None ):
+
+            # value is not None.
+            value_OUT = int( value_OUT )
+            
+        #-- END check to see if default was None --#
         
         return value_OUT
     
@@ -217,11 +301,11 @@ class DictHelper( object ):
     
 
     @classmethod
-    def get_dict_value_as_str( cls, dict_IN, name_IN, default_IN = -1 ):
+    def get_dict_value_as_str( cls, dict_IN, name_IN, default_IN = "" ):
     
         '''
         Accepts dictionary, name, and optional default value (if no default
-           provided, default is -1).  If dictionary or name missing, returns
+           provided, default is "").  If dictionary or name missing, returns
            default.  If name not present in dictionary, returns default.  If name
            in dictionary, returns whatever is mapped to name, converted to a
            string through call to str().
@@ -229,7 +313,7 @@ class DictHelper( object ):
         Parameters:
         - dict_IN - dictionary we are looking in.
         - name_IN - name we are looking for in dictionary.
-        - default_IN (defaults to None) - default value to return if problem or not found in dict.
+        - default_IN (defaults to "") - default value to return if problem or not found in dict.
         
         Returns:
         - value_OUT - value mapped to name_IN in dict_IN, converted to str(), else the default value if problems or if not found in dict.
@@ -241,8 +325,13 @@ class DictHelper( object ):
         # first, get value.
         value_OUT = cls.get_dict_value( dict_IN, name_IN, default_IN )
         
-        # convert to str.
-        value_OUT = str( value_OUT )
+        # convert to str unless value is None.
+        if ( value_OUT != None ):
+
+            # value is not None.
+            value_OUT = str( value_OUT )
+            
+        #-- END check to see if default was None --#
         
         return value_OUT
     
@@ -372,11 +461,374 @@ class DictHelper( object ):
     #-- END function set_dict_value --#
 
 
-#-- END class DictHelper --#
+    #============================================================================
+    # ! ==> Built-in Instance methods
+    #============================================================================
+
+
+    def __init__( self, *args, **kwargs ):
+        
+        # initialize variables
+        self.m_dictionary = {}
+
+    #-- END method __init__() --#
+
+
+    #============================================================================
+    # ! ==> Instance methods
+    #============================================================================
+
+
+    def get_dictionary( self ):
+    
+        # return reference
+        value_OUT = None
+        
+        # declare variables
+        dict_instance = None
+        
+        # get m_dictionary
+        value_OUT = self.m_dictionary
+        
+        # got anything?
+        if ( value_OUT is None ):
+        
+            # make dictionary instance.
+            dict_instance = {}
+            
+            # store the instance.
+            self.set_dictionary( dict_instance )
+            
+            # get the instance.
+            value_OUT = self.get_dictionary()
+        
+        #-- END check to see if dictionary initialized. --#
+        
+        return value_OUT
+    
+    #-- END method get_dictionary --#
+
+
+    def get_value( self, name_IN, default_IN = None ):
+    
+        '''
+        Accepts name, and optional default value (if no default
+           provided, default is None).  If dictionary or name missing, returns
+           default.  If name not present in dictionary, returns default.  If name
+           in dictionary, returns whatever is mapped to name.
+           
+        Parameters:
+        - name_IN - name we are looking for in dictionary.
+        - default_IN (defaults to None) - default value to return if problem or not found in dict.
+        
+        Returns:
+        - value_OUT - value mapped to name_IN in dict_IN, else the default value if problems or if not found in dict.
+        '''
+    
+        # return reference
+        value_OUT = None
+        
+        # declare variables
+        my_dictionary = None        
+        
+        # get dictionary
+        my_dictionary = self.get_dictionary()
+
+        # call corresponding class method.
+        value_OUT = DictHelper.get_dict_value( my_dictionary, name_IN, default_IN )
+                
+        return value_OUT
+    
+    #-- END function get_value --#
+
+
+    def get_value_as_decimal( self, name_IN, default_IN = decimal.Decimal( -1 ) ):
+    
+        '''
+        Accepts name, and optional default value (if no default
+           provided, default is -1).  If dictionary or name missing, returns
+           default.  If name not present in dictionary, returns default.  If name
+           in dictionary, returns whatever is mapped to name, converted to a
+           decimal.Decimal instance through call to decimal.Decimal().
+           
+        Parameters:
+        - name_IN - name we are looking for in dictionary.
+        - default_IN (defaults to None) - default value to return if problem or not found in dict.
+        
+        Returns:
+        - value_OUT - value mapped to name_IN in dict_IN, converted to decimal.Decimal, else the default value if problems or if not found in dict.
+        '''
+    
+        # return reference
+        value_OUT = None
+        
+        # declare variables
+        me = "get_value_as_decimal"
+        my_dictionary = None
+        
+        # get dictionary
+        my_dictionary = self.get_dictionary()
+
+        # call corresponding class method.
+        value_OUT = DictHelper.get_dict_value_as_decimal( my_dictionary, name_IN, default_IN )
+
+        return value_OUT
+    
+    #-- END function get_value_as_decimal --#
+
+
+    def get_value_as_float( self, name_IN, default_IN = float( -1 ) ):
+    
+        '''
+        Accepts name, and optional default value (if no default
+           provided, default is -1).  If dictionary or name missing, returns
+           default.  If name not present in dictionary, returns default.  If name
+           in dictionary, returns whatever is mapped to name, converted to a
+           float through call to float().
+           
+        Parameters:
+        - name_IN - name we are looking for in dictionary.
+        - default_IN (defaults to None) - default value to return if problem or not found in dict.
+        
+        Returns:
+        - value_OUT - value mapped to name_IN in dict_IN, converted to float, else the default value if problems or if not found in dict.
+        '''
+    
+        # return reference
+        value_OUT = None
+        
+        # declare variables
+        me = "get_value_as_float"
+        my_dictionary = None
+        
+        # get dictionary
+        my_dictionary = self.get_dictionary()
+
+        # call corresponding class method.
+        value_OUT = DictHelper.get_dict_value_as_float( my_dictionary, name_IN, default_IN )
+
+        return value_OUT
+    
+    #-- END function get_value_as_float --#
+
+
+    def get_value_as_int( self, name_IN, default_IN = -1 ):
+    
+        '''
+        Accepts name, and optional default value (if no default
+           provided, default is -1).  If dictionary or name missing, returns
+           default.  If name not present in dictionary, returns default.  If name
+           in dictionary, returns whatever is mapped to name, converted to an
+           integer through call to int().
+           
+        Parameters:
+        - name_IN - name we are looking for in dictionary.
+        - default_IN (defaults to None) - default value to return if problem or not found in dict.
+        
+        Returns:
+        - value_OUT - value mapped to name_IN in dict_IN, converted to int(), else the default value if problems or if not found in dict.
+        '''
+    
+        # return reference
+        value_OUT = None
+        
+        # declare variables
+        me = "get_value_as_int"
+        my_dictionary = None
+        
+        # get dictionary
+        my_dictionary = self.get_dictionary()
+
+        # call corresponding class method.
+        value_OUT = DictHelper.get_dict_value_as_int( my_dictionary, name_IN, default_IN )
+
+        return value_OUT
+    
+    #-- END function get_value_as_int --#
+
+
+    def get_value_as_list( self, name_IN, default_IN = [], delimiter_IN = ',' ):
+        
+        # return reference
+        list_OUT = []
+        
+        # declare variables
+        me = "get_value_as_list"
+        my_dictionary = None
+        list_param_value = ""
+
+        # get dictionary
+        my_dictionary = self.get_dictionary()
+
+        # call corresponding class method.
+        list_OUT = DictHelper.get_dict_value_as_list( my_dictionary, name_IN, default_IN, delimiter_IN )
+        
+        return list_OUT
+        
+    #-- END method get_value_as_list() --#
+    
+
+    def get_value_as_str( self, name_IN, default_IN = "" ):
+    
+        '''
+        Accepts name, and optional default value (if no default
+           provided, default is "").  If dictionary or name missing, returns
+           default.  If name not present in dictionary, returns default.  If name
+           in dictionary, returns whatever is mapped to name, converted to a
+           string through call to str().
+           
+        Parameters:
+        - name_IN - name we are looking for in dictionary.
+        - default_IN (defaults to "") - default value to return if problem or not found in dict.
+        
+        Returns:
+        - value_OUT - value mapped to name_IN in my_dictionary, converted to str(), else the default value if problems or if not found in dict.
+        '''
+    
+        # return reference
+        value_OUT = None
+        
+        # declare variables
+        my_dictionary = None
+        
+        # call class method with nested dictionary.
+        my_dictionary = self.get_dictionary()
+        value_OUT = DictHelper.get_dict_value_as_str( my_dictionary, name_IN, default_IN )
+        
+        return value_OUT
+    
+    #-- END function get_value_as_str --#
+
+
+    def increment_int_value( self, name_IN, value_IN = 1, *args, **kwargs ):
+        
+        '''
+        Accepts name, and optional increment value.  Increment value defaults to
+           1.  Retrieves value as int, adds value_IN to it, then stores and
+           returns the result.  Returns None if error.
+        '''
+        
+        # return reference
+        value_OUT = None
+        
+        # declare variables
+        my_dictionary = None
+        my_value = -1
+        
+        # got dict?
+        my_dictionary = self.get_dictionary()
+        if ( cls.is_dict( my_dictionary ) == True ):
+        
+            # yes.  Got name?
+            if ( ( name_IN ) and ( name_IN != None ) ):
+            
+                # yes.  Retrieve value for name as int.
+                my_value = self.get_value_as_int( name_IN )
+                
+                # add value_IN
+                my_value += value_IN
+                
+                # store result and set return value
+                value_OUT = cls.set_value( name_IN, my_value )
+            
+            else:
+            
+                # no name.  error - return None
+                value_OUT = None
+            
+            #-- END check to see if name --#
+
+        else:
+        
+            # error - no dict.  Return None.
+            value_OUT = None
+        
+        #-- END check to see if dict. --#
+        
+        return value_OUT
+        
+    #-- END method increment_int_value() --#
+
+
+    def set_dictionary( self, instance_IN ):
+        
+        '''
+        Accepts dictionary.  Stores it and returns it.
+        '''
+        
+        # return reference
+        value_OUT = None
+        
+        # use store dictionary.
+        self.m_dictionary = instance_IN
+        
+        # return it.
+        value_OUT = self.m_dictionary
+        
+        return value_OUT
+        
+    #-- END method set_dictionary() --#
+
+
+    def set_value( self, name_IN, value_IN ):
+    
+        '''
+        Accepts name, and value.  If dictionary or name missing,
+           returns error message prefixed by STATUS_ERROR_PREFIX.  If dict and
+           name set, places value_IN, whatever it is, in dict with name as key.
+           
+        Parameters:
+        - name_IN - name we are looking for in dictionary.
+        - value_IN - value to associate in dict with name passed in.
+        
+        Returns:
+        - value_OUT - value mapped to name_IN in m_dictionary, else an error message if problems.  To check for success, check if value_IN = value_OUT.  If not, error.
+        '''
+    
+        # return reference
+        value_OUT = None
+        
+        # declare variables
+        my_dictionary = None
+        
+        # get nested dictionary
+        my_dictionary = self.m_dictionary
+        
+        # first, make sure we have all the stuff we need.  If stuff missing, return
+        #    default.
+        if ( DictHelper.is_dict( my_dictionary ) == True ):
+        
+            # got dictionary.  Got name?
+            if ( ( name_IN ) and ( name_IN != None ) ):
+            
+                # yup - store value.
+                my_dictionary[ name_IN ] = value_IN
+                value_OUT = self.get_value( name_IN )            
+
+            else:
+            
+                # no name.  Return error.
+                value_OUT = DictHelper.STATUS_ERROR_PREFIX + "No name passed in, can't set value."
+            
+            #-- END check to see if name. --#
+        
+        else:
+        
+            # no dictionary.  Return error.
+            value_OUT = DictHelper.STATUS_ERROR_PREFIX + "No dictionary passed in, can't set value."
+            
+        #-- END check to see if dictionary passed in. --#
+        
+        return value_OUT
+    
+    #-- END function set_value --#
+
+
+#-- END class LoggingHelper --##-- END class DictHelper --#
 
 
 #================================================================================
-# functions
+# ! functions
 #================================================================================
 
 def get_dict_value( dict_IN, name_IN, default_IN = None ):
