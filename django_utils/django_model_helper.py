2b2aa1a95096a71a4b5437982a218cc1862e4d08
Jonathan Morgan <jonathan.scott.morgan@gmail.com>
2016-09-25 02:17:56 -0400

    added method to copy ManyToMany values from one instance to another within same model class.

--- django_utils/django_model_helper.py
+++ django_utils/django_model_helper.py
@@ -21,6 +21,9 @@ along with http://github.com/jonathanmorgan/python_utilities.  If not, see
 # start to python 3 support:
 from __future__ import unicode_literals
 
+# imports - python_utilities
+from python_utilities.status.status_container import StatusContainer
+
 class DjangoModelHelper( object ):
     
 
@@ -47,5 +50,97 @@ class DjangoModelHelper( object ):
                                    CLASS_NAME_M_TO_M_OLD,
                                    CLASS_NAME_M_TO_M_NEW ]
 
+    #===========================================================================
+    # ! ==> class methods
+    #===========================================================================
+
+
+    @classmethod
+    def copy_m2m_values( cls, attr_name_IN, copy_from_IN, copy_to_IN, *args, **kwargs ):
+        
+        '''
+        Accepts the name of the ManyToMany (m2m) field you want to copy from
+            one instance of a given model class to another, the instance you
+            want to copy from, and the instance you want to copy to.  Loops over
+            items in the from instance for the field and adds each to the field
+            in the to instance.  If error returns status of error and message
+            that describes the problem.
+            
+        Postconditions: the copy-to instance is updated, but not saved.  If 
+            error returned, the copy-to instance is not updated.
+        '''
+        
+        # return reference
+        status_OUT = None
+        
+        # declare variables
+        me = "copy_m2m_values"
+        status_message = ""
+        copy_m2m_from = None
+        copy_m2m_to = None
+        m2m_qs = None
+        m2m_instance = None
+        
+        # init status container
+        status_OUT = StatusContainer()
+        status_OUT.set_status_code( StatusContainer.STATUS_CODE_SUCCESS )
+        
+        # got an attribute name?
+        if ( ( attr_name_IN is not None ) and ( attr_name_IN != "" ) ):
+        
+            # got copy_from_IN instance?
+            if ( copy_from_IN is not None ):
+            
+                # got copy_to_IN instance?
+                if ( copy_to_IN is not None ):
+                
+                    # ! ----> ManyToMany - topics
+                    copy_m2m_from = getattr( copy_from_IN, attr_name_IN )
+                    copy_m2m_to = getattr( copy_to_IN, attr_name_IN )
+                    
+                    # loop over existing items
+                    m2m_qs = copy_m2m_from.all()
+                    for m2m_instance in m2m_qs:
+                    
+                        # add to new.
+                        copy_m2m_to.add( m2m_instance )
+                    
+                    #-- END loop over m2m instances --#
+                    
+                else:
+                
+                    # error - must have attribute name.
+                    status_OUT.set_status_code( StatusContainer.STATUS_CODE_ERROR )
+            
+                    # add a status message
+                    status_message = "No instance passed in to copy TO."
+                    status_OUT.add_message( status_message )
+                
+                #-- END check to see if copy_to_IN --#
+    
+            else:
+            
+                # error - must have attribute name.
+                status_OUT.set_status_code( StatusContainer.STATUS_CODE_ERROR )
+        
+                # add a status message
+                status_message = "No instance passed in to copy FROM."
+                status_OUT.add_message( status_message )
+            
+            #-- END check to see if copy_from_IN --#
+            
+        else:
+        
+            # error - must have attribute name.
+            status_OUT.set_status_code( StatusContainer.STATUS_CODE_ERROR )
+    
+            # add a status message
+            status_message = "No attribute name passed in ( " + str( attr_name_IN ) + " )"
+            status_OUT.add_message( status_message )
+            
+        #-- END check for attribute name --#
+        
+    #-- END class method copy_m2m_values() --#
+    
 
 #-- END class DjangoModelHelper --#
